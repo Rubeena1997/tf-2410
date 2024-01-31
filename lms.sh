8bcbfb3481e635bee6d808fcd344891b6b820b61
Rubeena1997 <rubeenazabin3355@gmail.com>
2024-01-31 15:07:49 +0530

    npm changes

--- lms.sh
+++ lms.sh
@@ -3,4 +3,13 @@ sudo apt update -y
 sudo apt install nginx -y
 sudo rm -rf /var/www/html*
 sudo git clone https://github.com/Rubeena1997/lms.git
-sudo cp -r /lms/webapp/dist/* /var/www/html
+cd lms
+cd webapp
+# Add the NodeSource repository for Node.js 16.x
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
+# Install Node.js and npm
+sudo apt-get install -y nodejs
+npm install
+npm run build
+sudo cp -r dist/* /var/www/html
