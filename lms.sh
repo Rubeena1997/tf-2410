8bbffe25e4a85cf748af244b9da70481856a87a5
Rubeena1997 <rubeenazabin3355@gmail.com>
2024-01-31 11:08:06 +0530

    create ec2 and host app

---
+++ lms.sh
@@ -0,0 +1,6 @@
+sudo apt update -y
+sudo apt install nginx -y
+sudo ss -ntpl
+sudo rm -rf /var/www/html*
+sudo git clone https://github.com/Rubeena1997/lms.git
+sudo cp -r /lms/webapp/dist/* /var/www/html
