855e6520dc14555ae091d46c29b64b931c1c846f
Rubeena1997 <rubeenazabin3355@gmail.com>
2024-01-31 18:21:42 +0530

    lms app

--- lms.sh
+++ lms.sh
@@ -3,12 +3,23 @@ sudo apt update -y
 sudo apt install nginx -y
 sudo git clone https://github.com/Rubeena1997/lms.git
 cd lms
-cd webapp
+
+# frontend deployment
+
+sudo rm -f /var/www/html/*
+sudo cp -rf webapp/* /var/www/html/
+sudo systemctl restart nginx
+echo "frontend deployed"
+
+
+
+
+#cd webapp
 # Add the NodeSource repository for Node.js 16.x
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+# curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 
 # Install Node.js and npm
-sudo apt-get install -y nodejs
-npm install
-npm run build
-sudo cp -r dist/* /var/www/html
+# sudo apt-get install -y nodejs
+# npm install
+#npm run build
+# sudo cp -r dist/* /var/www/html
