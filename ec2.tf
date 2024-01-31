976aea9a45be41f097da6fb0cbb5ec3588466dfa
Rubeena1997 <rubeenazabin3355@gmail.com>
2024-01-31 11:29:22 +0530

    key

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-08e2c1a8d17c2fe17"
   instance_type = "t2.micro"
-  key_name = "rubi.pem"
+  key_name = "rubi"
   subnet_id     = aws_subnet.myntra-web-sn.id
   vpc_security_group_ids = [aws_security_group.myntra-web-sg.id]
  ## user_data = file("lms.sh")
