cf72a5eb69fd7a54c8513f1f859cccea5ec394d2
Rubeena1997 <rubeenazabin3355@gmail.com>
2024-01-31 11:44:00 +0530

    clone

--- lms.sh
+++ lms.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install nginx -y
-sudo ss -ntpl
 sudo rm -rf /var/www/html*
 sudo git clone https://github.com/Rubeena1997/lms.git
 sudo cp -r /lms/webapp/dist/* /var/www/html
