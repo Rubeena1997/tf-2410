46a3e3f65ad0362482410815ad9afab847c89971
Rubeena1997 <rubeenazabin3355@gmail.com>
2024-01-31 15:38:28 +0530

    ec2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-08e2c1a8d17c2fe17"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "rubi"
   subnet_id     = aws_subnet.myntra-web-sn.id
   vpc_security_group_ids = [aws_security_group.myntra-web-sg.id]
